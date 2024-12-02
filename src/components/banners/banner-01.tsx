f1753547443fcbbd4eff5815d1511654e3bc52fa
sahilkumardev <sahilkumar710@outlook.com>
2024-12-02 11:42:44 +0530

    add banner one

--- src/components/banners/banner-01.tsx
+++ src/components/banners/banner-01.tsx
@@ -1,22 +1,21 @@
-// Dependencies: pnpm install lucide-react
-
+import { Card } from "@/components/ui/card";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function BannerDemo() {
   return (
-    <div className="dark bg-muted px-4 py-3 text-foreground">
-      <p className="flex justify-center text-sm">
-        <a href="#" className="group">
-          <span className="me-1 text-base leading-none">✨</span>
-          Introducing transactional and marketing emails
-          <ArrowRight
-            className="-mt-0.5 ms-2 inline-flex opacity-60 transition-transform group-hover:translate-x-0.5"
-            size={16}
-            strokeWidth={2}
-            aria-hidden="true"
-          />
-        </a>
-      </p>
-    </div>
+    <Link href="#">
+      <Card className="px-8 py-4 flex items-center justify-center ">
+        <span className="line-clamp-1">
+          ✨ Introducing transactional and marketing emails
+        </span>
+        <ArrowRight
+          className="ms-2 inline-flex opacity-60 transition-transform group-hover:translate-x-0.5"
+          size={16}
+          strokeWidth={2}
+          aria-hidden="true"
+        />
+      </Card>
+    </Link>
   );
 }
